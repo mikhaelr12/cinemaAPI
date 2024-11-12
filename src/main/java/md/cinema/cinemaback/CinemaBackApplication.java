6b195c99154e04ef02f3ae7d533bdd1e9c5d1ab8
Mihai Rusu <rusumiihai@gmail.com>
2024-11-12 22:03:31 +0200

    initial commit

---
+++ src/main/java/md/cinema/cinemaback/CinemaBackApplication.java
@@ -0,0 +1,13 @@
+package md.cinema.cinemaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBackApplication.class, args);
+    }
+
+}
