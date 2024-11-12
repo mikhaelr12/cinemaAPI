48e90bbb2a4e49e73dc027fdfbbd2387e73ac233
Mihai Rusu <rusumiihai@gmail.com>
2024-11-12 22:18:27 +0200

    implemented jwt

---
+++ src/main/java/md/cinema/cinemaback/entity/User.java
@@ -0,0 +1,40 @@
+package md.cinema.cinemaback.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import md.cinema.cinemaback.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name())); // Map role to authority
+    }
+}
---
+++ src/main/java/md/cinema/cinemaback/enums/Status.java
@@ -0,0 +1,6 @@
+package md.cinema.cinemaback.enums;
+
+public enum Status {
+    IN_PROGRESS,
+    COMPLETED,
+}
